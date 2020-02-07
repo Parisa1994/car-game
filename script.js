ee66918bf18d8fe89576cbd1ff00a76c117a7054
Parisa Zamani <parisa.zmni1994@gmail.com>
2020-02-07 18:20:15 +0330

    add arrow in gamestart

--- script.js
+++ script.js
@@ -1,26 +1,55 @@
-const score = document.querySelector('.score');
-const startScreen = document.querySelector('.startScreen');
-const gameArea = document.querySelector('.gameArea');
-let keys = {
-    ArrowUp: false,ArrowDown: false,ArrowRight: false,ArrowLeft: false
+const score = document.querySelector(".score");
+const startScreen = document.querySelector(".startScreen");
+const gameArea = document.querySelector(".gameArea");
+let player = {
+    speed: 5
 };
-
+let keys = {
+    ArrowUp:false,
+    ArrowDown:false,
+    ArrowRight:false,
+    ArrowLeft:false
+};
 startScreen.addEventListener("click", start);
 document.addEventListener("keydown", pressOn);
 document.addEventListener("keyup", pressOff);
 
-function pressOff (e){
-    e.preventDefault();
-    keys[e.key] = true;
-    console.log(keys);
+function playGame(){
+    console.log("inplay");
+    let car = document.querySelector(".car");
+    if(player.start){
+        if(keys.ArrowUp){ player.y -= player.speed; }
+        if(keys.ArrowDown){ player.y += player.speed; }
+        if(keys.ArrowRight){ player.x += player.speed; }
+        if(keys.ArrowLeft){ player.x -= player.speed; }
+        car.style.left = player.x + 'px';
+        car.style.top = player.y + 'px';
+        window.requestAnimationFrame(playGame);
+    }
 }
-
 function pressOn(e){
     e.preventDefault();
     keys[e.key] = true;
     console.log(keys);
 }
 
+function pressOff(e){
+    e.preventDefault();
+    keys[e.key] = false;
+    console.log(keys);
+}
+
+
 function start(){
-    console.log("click");
+    startScreen.classList.add("hide");
+    gameArea.classList.remove("hide");
+    player.start = true;
+    window.requestAnimationFrame(playGame);
+    let car = document.createElement("div");
+    car.innerText = "car";
+    car.setAttribute("class", "car");
+    gameArea.appendChild(car);
+    player.x = car.offsetLeft;
+    player.y = car.offsetTop;
+    console.log(player);
 }
