4a80ab1066cc117fa89ff6906ffa6c611385aa4a
Parisa Zamani <parisa.zmni1994@gmail.com>
2020-02-08 13:36:37 +0330

    add linemoving

--- script.js
+++ script.js
@@ -13,10 +13,21 @@ let keys = {
 startScreen.addEventListener("click", start);
 document.addEventListener("keydown", pressOn);
 document.addEventListener("keyup", pressOff);
-
+function moveLines(){
+    let lines = document.querySelectorAll(".line");
+    lines.forEach(function(item){
+        console.log(item.y);
+        if(item.y>1500){
+            item.y -= 1500;
+        }
+        item.y += player.speed;
+        item.style.top = item.y + "px";
+    });
+}
 function playGame(){
     console.log("inplay");
     let car = document.querySelector(".car");
+    moveLines();
     if(player.start){
         if(keys.ArrowUp){ player.y -= player.speed; }
         if(keys.ArrowDown){ player.y += player.speed; }
@@ -32,17 +43,21 @@ function pressOn(e){
     keys[e.key] = true;
     console.log(keys);
 }
-
 function pressOff(e){
     e.preventDefault();
     keys[e.key] = false;
     console.log(keys);
 }
-
-
 function start(){
     startScreen.classList.add("hide");
     gameArea.classList.remove("hide");
+    for(let x=0;x<10;x++){
+        let div = document.createElement("div");
+        div.classList.add("line");
+        div.y = x*150;
+        div.style.top = (x*150)+"px";
+        gameArea.appendChild(div);
+    }
     player.start = true;
     window.requestAnimationFrame(playGame);
     let car = document.createElement("div");
