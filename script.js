3c21e6260e79e9936eb164aba61da21bdfa94a6f
Parisa Zamani <parisa.zmni1994@gmail.com>
2020-02-08 16:14:09 +0330

    add car enemy in game

--- script.js
+++ script.js
@@ -24,10 +24,25 @@ function moveLines(){
         item.style.top = item.y + "px";
     });
 }
+function moveEnemy(car){
+    let ele = document.querySelectorAll(".enemy");
+    if(isCollide(car, item)){
+        console.log("HIT")
+    }
+    ele.forEach(function(item){
+        if(item.y>1500){
+            item.y = -600;
+            item.style.left = Math.floor(Math.random()*150)+"px";
+        }
+        item.y += player.speed;
+        item.style.top = item.y + "px";
+    });
+}
 function playGame(){
     console.log("inplay");
     let car = document.querySelector(".car");
     moveLines();
+    moveEnemy(car);
     if(player.start){
         if(keys.ArrowUp){ player.y -= player.speed; }
         if(keys.ArrowDown){ player.y += player.speed; }
@@ -36,6 +51,16 @@ function playGame(){
         car.style.left = player.x + 'px';
         car.style.top = player.y + 'px';
         window.requestAnimationFrame(playGame);
+    }
+    function isCollide(a, b){
+        let aRect = a.getBoundingClientRect();
+        let bRect = b.getBoundingClientRect();
+        return!(
+            (aRect.bottom < bRect.top)||
+            (aRect.top > bRect.bottom)||
+            (aRect.right < bRect.left)||
+            (aRect.left > bRect.right)
+        )
     }
 }
 function pressOn(e){
@@ -66,5 +91,13 @@ function start(){
     gameArea.appendChild(car);
     player.x = car.offsetLeft;
     player.y = car.offsetTop;
-    console.log(player);
+    for(let x=0; x<3; x++){
+        let enemy = document.createElement("div");
+        enemy.classList.add("enemy");
+        enemy.y = ((x+1)*600)*-1;
+        enemy.style.top = enemy.y +"px";
+        enemy.style.left = Math.floor(Math.random()*800)+"px";
+        enemy.style.backgroundColor = "red";
+        gameArea.appendChild(enemy);
+    }
 }
