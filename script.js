e0d82fab76f8833dcf365bb119c6c95009bcf92e
Parisa Zamani <parisa.zmni1994@gmail.com>
2020-02-10 15:37:04 +0330

    add diftrent color car

--- script.js
+++ script.js
@@ -107,10 +107,19 @@ function start(){
     for(let x=0; x<3; x++){
         let enemy = document.createElement("div");
         enemy.classList.add("enemy");
+        enemy.innerHTML = (x+1);
         enemy.y = ((x+1)*600)*-1;
         enemy.style.top = enemy.y +"px";
         enemy.style.left = Math.floor(Math.random()*800)+"px";
-        enemy.style.backgroundColor = "red";
+        enemy.style.backgroundColor = randomColor();
         gameArea.appendChild(enemy);
     }
+}
+function randomColor(){
+    function c(){
+        let hex = Math.floor(Math.random()*256).toString(16);
+        return("0"+String(hex)).substr(-2)
+    }
+    return "#"+c()+c()+c();
+
 }
